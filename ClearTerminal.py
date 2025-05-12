e58fd5f4ada9403ffb8065b6ef1fe9240641b385
Mukhammadjon <mukhammadjontursunaliev42@gmail.com>
2025-05-12 19:00:09 +0200

    Third commit

---
+++ ClearTerminal.py
@@ -0,0 +1,16 @@
+#Clear terminal 
+
+import time
+import os
+
+#From os module it shows list files
+os.system("ls")
+
+#System will sleep for 2 seconds
+time.sleep(2)
+
+#From os system it will clear all history 
+
+os.system('clear')
+
+
